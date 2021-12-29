a0492640214e8a6f9dc29212d3c1db4f7649cd1a
JMinkyoung <whk5817@gmail.com>
2021-12-29 21:14:05 +0900

    Timer Clock 구현

--- ocular-timer/src/components/TimerClock.tsx
+++ ocular-timer/src/components/TimerClock.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import { useMediaQuery } from 'react-responsive';
 import styled from 'styled-components';
 
@@ -22,30 +22,36 @@ const ClockWrapper = styled.div`
   }
 `;
 
-const ClockCircle = styled.div`
+const ClockCircleWrapper = styled.svg<{time: number, color: string}>`
   position: relative;
   width: 700px;
   height: 700px;
   margin: auto;
-  background: white;
-
-  // 오른쪽
-  background-image: linear-gradient(90deg, transparent 50%, #22577E 50%), linear-gradient(90deg, #22577E 50%, transparent 50%);
-  /* background-image: linear-gradient(135deg, transparent 50%, #22577E 50%), linear-gradient(90deg, #22577E 50%, transparent 50%); */
-  /* background-image: linear-gradient(180deg, transparent 50%, #22577E 50%), linear-gradient(90deg, #22577E 50%, transparent 50%); */
-  /* background-image: linear-gradient(90deg, #22577E 50%, transparent 50%);  */
-
-
-  // 왼쪽
-  /* background-image: linear-gradient(90deg, transparent 50%, white 50%), linear-gradient(90deg, #22577E 50%, transparent 50%); */
-  /* background-image: linear-gradient(135deg, transparent 50%, white 50%), linear-gradient(90deg, #22577E 50%, transparent 50%); */
-  /* background-image: linear-gradient(180deg, transparent 50%, white 50%), linear-gradient(90deg, #22577E 50%, transparent 50%); */
-
-
-  border-radius: 100%;
+  transform: rotate(-90deg);
   @media ${(props) => props.theme.mobile} {
     width: 350px;
     height: 350px;
+  }
+`;
+
+const ClockCircle = styled.circle<{time: number, done: boolean, radius: number, circumference: number}>`
+  fill: none;
+  stroke: #ffff;
+  cx: 350;
+  cy: 350;
+  r: 150;
+  stroke-width: 300;
+  stroke-dasharray: ${(props)=>props.circumference};
+  animation: ${(props)=> props.done ? null : `dash ${props.time}s linear infinite`};
+  visibility: ${(props)=> props.done ? 'hidden' : 'visible'};
+
+  @keyframes dash {
+    from{
+      stroke-dashoffset: ${(props)=>props.circumference};
+    }
+    to {
+      stroke-dashoffset:0;
+    }
   }
 `;
 
@@ -53,10 +59,33 @@ const ClockCircle = styled.div`
 
 
 const TimerClock = () => {
+  const [time, setTime] = useState(0);
+  const [done, setDone] = useState(false);
+  const target: number = 60;
+  const timeRef:any = useRef();
+  const radius = 150;
+  const circumference = 2 * Math.PI * radius;
+
+  useEffect(()=>{
+    const tick = () => {
+      setTime(prevTime => prevTime+1);
+    }
+    timeRef.current = setInterval(()=> tick(), 1000); 
+  },[]);
+
+  useEffect(()=>{
+    if(time === target){
+      clearInterval(timeRef.current);
+      setDone(true);
+    }
+  });
+
   return (
     <ClockWrapper>
-      
-    <ClockCircle/>
+    <ClockCircleWrapper time={time} color={"22577E"}>
+      <circle cx="350" cy="350" r="299" fill="#22577E" />
+      <ClockCircle time={target} done={done} radius={radius} circumference={circumference} />
+    </ClockCircleWrapper>
     </ClockWrapper>
   );
 }
