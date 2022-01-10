8cfa116e8204a91d94d3c0f7d366010a4b73df98
JMinkyoung <whk5817@gmail.com>
2022-01-10 21:59:47 +0900

    Audio Player Add

--- ocular-timer/src/pages/index.tsx
+++ ocular-timer/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import React from 'react';
 import TimerClock from '../components/TimerClock';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css'; 
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 import { RootState } from '../modules';
@@ -17,7 +19,8 @@ const ClockComponentWrapper = styled.div`
   align-items: center;
   justify-content:center;
   width: 100%;
-  height: 100%;
+  margin-top:50px;
+  height: auto;
 `;
 
 type TimeDataType = {
@@ -40,11 +43,10 @@ const Home: NextPage = () => {
     {/* 여기서 redux를 통해서 저장된 타이머 정보를 가져올 예정 */}
       <ClockComponentWrapper>
         <span style={{marginBottom: "20px", fontSize: "2rem"}}>{timerData[0].title}</span>
-        {/* <span style={{marginBottom: "20px", fontSize: "1.3rem"}}>{timerData[0].time[0]["subtitle"]}</span> */}
         <TimerClock timeData={timerData[0]}/>
-
-        <div style={{width: "300px", height: "20px", backgroundColor: "grey", marginTop: "50px"}}>여기에 음악 플레이어 들어감</div>
       </ClockComponentWrapper>
+      <AudioPlayer autoPlay={false} loop src="/Book Bag.mp3"/>
+
     </PageWrapper>
   )
 }
