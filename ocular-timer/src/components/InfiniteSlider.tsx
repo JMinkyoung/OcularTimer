36a2a63bfef1697a680276b620f7bb135727494d
JMinkyoung <whk5817@gmail.com>
2022-02-25 16:23:49 +0900

    Infinite Slide 구현 완료
    Pagenation, auto slide 구현해야함

--- ocular-timer/src/components/InfiniteSlider.tsx
+++ ocular-timer/src/components/InfiniteSlider.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect , useRef} from 'react';
 import styled from 'styled-components';
 
 interface Iprops {
@@ -17,9 +17,9 @@ const SliderWrapper = styled.div`
   }
 `;
 const SliderList = styled.div`
-  width: 4000px;
+  width: 6000px;
   @media ${(props) => props.theme.mobile} {
-    width: 1400px;
+    width: 2100px;
   }
 `;
 const SliderBox = styled.div`
@@ -40,16 +40,20 @@ const SliderContent = styled.div`
 `;
 const InfiniteSlider = (props: Iprops) => {
   const [curIdx, setCurIdx] = useState(0);
+  const [transition, setTransition] = useState("");
+
   const slideWidth: number = props.type==="pc" ? 1000 : 350;
-  const transitionSeppd = 500;
-  const transitionStyle = `transform ${transitionSeppd}ms ease 0s`;
+  const transitionSpeed = 500;
+  const transitionStyle = `transform ${transitionSpeed}ms ease 0s`;
 
   const rednering = () => {
     const result = [];
     if(props.type==="pc"){
+      result.push(<SliderContent><img key={-1} style={{width:"100%"}}src={`/img/pcex4.JPG`}/></SliderContent>)
       for(let i: number = 1; i<5; i++){
         result.push(<SliderContent><img key={i} style={{width:"100%"}}src={`/img/pcex${i}.JPG`}/></SliderContent>);
       }
+      result.push(<SliderContent><img key={5} style={{width:"100%"}}src={`/img/pcex1.JPG`}/></SliderContent>)
     }else{
       for(let i: number = 1; i<5; i++){
         result.push(<SliderContent><img key={i} src={`/img/mbex${i}.JPG`}/></SliderContent>);
@@ -58,26 +62,66 @@ const InfiniteSlider = (props: Iprops) => {
     return result;
   };
 
-  const onClickNext = (e: any) => {
-    e.preventDefault();
-    if(curIdx < 4){
+  // useEffect(()=>{
+  //   const interval = setInterval(()=>{
+  //     console.log("dd")
+  //     if(curIdx <= 3){
+  //       setCurIdx(prev => prev+1);
+  //       setTransition(transitionStyle);
+  //     }
+  //     if(curIdx === 3){
+  //       setTimeout(()=>{
+  //         setTransition("");
+  //         setCurIdx(0);
+  //       },transitionSpeed);
+  //       setTransition(transitionStyle);
+  //     }
+  //   },3000);
+  //   return () => {
+  //     clearInterval(interval);
+  //   }
+  // },[curIdx]);
+  // useEffect(()=>{
+  //   setTransition(transitionStyle);
+  //   setInterval(()=>{
+  //     onClickNext();
+  //   },3000);
+  //   return clearInterval();
+  // },[])
+
+  const onClickNext = () => {
+    console.log(curIdx, transition)
+    if(curIdx <= 3){
       setCurIdx(prev => prev+1);
+      setTransition(transitionStyle);
+    }
+    if(curIdx === 3){
+      setTimeout(()=>{
+        setTransition("");
+        setCurIdx(0);
+      },transitionSpeed);
     }
   }
 
   const onClickPrev = (e: any) => {
     e.preventDefault();
-    if(curIdx < 4){
+    if(curIdx >= 0){
       setCurIdx(prev => prev-1);
+      setTransition(transitionStyle);
+    }
+    if(curIdx === 0){
+      setTimeout(()=>{
+        setTransition("");
+        setCurIdx(3);
+      },transitionSpeed);
     }
   }
 
-  console.log(curIdx);
   return (
     <Container>
       <SliderWrapper>
         <SliderBox>
-          <SliderList style={{transform: `translateX(${(-slideWidth * curIdx)}px)`,transition: transitionStyle}}>
+          <SliderList style={{transform: `translateX(${(-slideWidth * (curIdx+1))}px)`,transition: transition}}>
             {rednering()}
           </SliderList>
         </SliderBox>
